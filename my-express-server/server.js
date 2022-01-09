6e4373aed492f2c003b14ff6ccce1a0ce7a39de8
ishita30 <ishitasinghal30@gmail.com>
2022-01-09 17:24:21 +0530

    Update server.js

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -6,6 +6,11 @@ app.get("/",function(request,response)
 {
   response.send("<h1>Hello World</h1>"); // we can send h1
 });
+
+app.get("/contact", function(req,res)
+{
+  res.send("Contact me at : ishitasinghal30@gmail.com");
+});
 function callback()
 {
   console.log("server started");
