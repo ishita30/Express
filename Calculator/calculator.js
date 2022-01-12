c799834b2b8b21ad5ce8b1e23eb856b36e4721e0
ishita30 <ishitasinghal30@gmail.com>
2022-01-12 15:44:35 +0530

    Update calculator.js

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,6 +1,9 @@
 //jshint esversion : 6
 const express=require("express");
+const bodyParser= require("body-parser");
+
 const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res)
 {
@@ -9,8 +12,11 @@ app.get("/",function(req,res)
 
 app.post("/", function(req,res)//to handle post requests
 {
+  console.log(req.body);
+
   res.send("thanks for posting that");
-})
+});
+
 function callBack()
 {
   console.log("Server is running");
